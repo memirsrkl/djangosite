e8e80b65ddb76acee1ecb1ce66693601526f53de
memirsrkl <79982341+memirsrkl@users.noreply.github.com>
2023-07-22 15:48:10 +0300

    Update settings.py

--- baykar/settings.py
+++ baykar/settings.py
@@ -16,7 +16,7 @@ env=environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ALLOWED_HOSTS = ['*']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
